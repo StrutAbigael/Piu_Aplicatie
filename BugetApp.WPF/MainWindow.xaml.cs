97435faef2a48e1cd6b9c687c87bd2d3b801b042
Strut Abigael <abistrut06@gmail.com>
2026-04-04 19:40:30 +0300

    Adaugare interfata WPF

---
+++ BugetApp.WPF/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BugetApp.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            AfiseazaDatePursePalette();
+        }
+
+        private void AfiseazaDatePursePalette()
+        {
+           
+            txtDescriere.Text = "Descriere: Programare salon";
+            txtSuma.Text = "Sumă: 250 RON";
+            txtTip.Text = "Tip: Cheltuială";
+            txtData.Text = "Data: " + DateTime.Now.ToString("dd/MM/yyyy");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
